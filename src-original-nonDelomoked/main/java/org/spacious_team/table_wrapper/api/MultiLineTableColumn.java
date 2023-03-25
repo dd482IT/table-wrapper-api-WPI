11c3d4d803de6929030552503acf6e766f945eb2
Daniel Daszkiewicz <dd482@njit.edu>
2023-03-25 11:55:17 -0400

    Delombok to get warnings

---
+++ src-original-nonDelomoked/main/java/org/spacious_team/table_wrapper/api/MultiLineTableColumn.java
@@ -0,0 +1,76 @@
+/*
+ * Table Wrapper API
+ * Copyright (C) 2020  Spacious Team <spacious-team@ya.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.spacious_team.table_wrapper.api;
+
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+import static lombok.AccessLevel.PRIVATE;
+
+/**
+ * Implements table header kind of
+ * <pre>
+ * |             One             |             Two            |
+ * |   a1    |   a2    |   a3    |   a1   |   a2    |   a3    |
+ * | b1 | b2 | b1 | b2 | b1 | b2 |b1 | b2 | b1 | b2 | b1 | b2 |
+ * <pre/>
+ * Can find index for (Two -> a3 -> b1) column
+ */
+@ToString
+@EqualsAndHashCode
+@RequiredArgsConstructor(access = PRIVATE)
+public class MultiLineTableColumn implements TableColumn {
+    private final TableColumn[] rowDescriptors;
+
+    /**
+     * @param rowDescriptors each array element describes next rows column
+     */
+    public static MultiLineTableColumn of(TableColumn... rowDescriptors) {
+        return new MultiLineTableColumn(rowDescriptors);
+    }
+
+    public static MultiLineTableColumn of(String... rowDescriptors) {
+        TableColumn[] descriptors = Arrays.stream(rowDescriptors)
+                .map(Objects::requireNonNull)
+                .map(PatternTableColumn::of)
+                .toArray(TableColumn[]::new);
+        return new MultiLineTableColumn(descriptors);
+    }
+
+    /**
+     * @param headerRows header rows count should be equal to count of row descriptors
+     */
+    @Override
+    public int getColumnIndex(int firstColumnForSearch, ReportPageRow... headerRows) {
+        if (headerRows.length != rowDescriptors.length) {
+            throw new TableColumnNotFound("Internal error, " + rowDescriptors.length + " rows expected in table header");
+        }
+        int columnIndex = firstColumnForSearch;
+        int i = 0;
+        for (ReportPageRow row : headerRows) {
+            TableColumn rowDescriptor = rowDescriptors[i++];
+            columnIndex = rowDescriptor.getColumnIndex(columnIndex, row);
+        }
+        return columnIndex;
+    }
+}
---
+++ src-original-nonDelomoked/test/java/org/spacious_team/table_wrapper/api/EmptyTableRowTest.java
@@ -0,0 +1,138 @@
+/*
+ * Table Wrapper API
+ * Copyright (C) 2022  Spacious Team <spacious-team@ya.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.spacious_team.table_wrapper.api;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static nl.jqno.equalsverifier.Warning.STRICT_INHERITANCE;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class EmptyTableRowTest {
+
+    @Mock
+    Table table;
+    @Mock
+    TableHeaderColumn column;
+    EmptyTableRow row;
+
+    @BeforeEach
+    void beforeEach() {
+        row = new EmptyTableRow(table, 0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 1000_000})
+    void getCell(int col) {
+        assertNull(row.getCell(col));
+    }
+
+    void testGetCell() {
+        assertNull(row.getCell(column));
+    }
+
+    void getFirstCellNum() {
+        assertEquals(-1, row.getFirstCellNum());
+    }
+
+    void getLastCellNum() {
+        assertEquals(-1, row.getLastCellNum());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "abc"})
+    void rowContains(String value) {
+        assertFalse(row.rowContains(value));
+    }
+
+    void iterator() {
+        assertFalse(row.iterator().hasNext());
+    }
+
+    void getCellValue() {
+        assertNull(row.getCellValue(column));
+    }
+
+    void getIntCellValue() {
+        assertThrows(NullPointerException.class,
+                () -> row.getIntCellValue(column));
+    }
+
+    void getLongCellValue() {
+        assertThrows(NullPointerException.class,
+                () -> row.getLongCellValue(column));
+    }
+
+    void getDoubleCellValue() {
+        assertThrows(NullPointerException.class,
+                () -> row.getDoubleCellValue(column));
+    }
+
+    void getBigDecimalCellValue() {
+        assertThrows(NullPointerException.class,
+                () -> row.getBigDecimalCellValue(column));
+    }
+
+    void getStringCellValue() {
+        assertThrows(NullPointerException.class,
+                () -> row.getStringCellValue(column));
+    }
+
+    void getInstantCellValue() {
+        assertThrows(NullPointerException.class,
+                () -> row.getInstantCellValue(column));
+    }
+
+    void getLocalDateTimeCellValue() {
+        assertThrows(NullPointerException.class,
+                () -> row.getLocalDateTimeCellValue(column));
+    }
+
+    void testClone() {
+        assertEquals(row, row.clone());
+    }
+
+    void testGetTable() {
+        assertEquals(table, row.getTable());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 1000_000})
+    void getRowNum(int rowNum) {
+        assertEquals(rowNum, new EmptyTableRow(table, rowNum).getRowNum());
+    }
+
+    void testEqualsAndHashCode() {
+        EqualsVerifier
+                .forClass(EmptyTableRow.class)
+                .suppress(STRICT_INHERITANCE) // no subclass for test
+                .verify();
+    }
+
+    void testToString() {
+        assertNotNull(row.toString());
+    }
+}
