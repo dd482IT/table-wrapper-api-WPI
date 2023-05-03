babe103b77e54f9c857f5c2720328bd5337d206b
Daniel Daszkiewicz <dd482@njit.edu>
2023-05-03 14:02:51 -0400

    Src delomboked, with annotations

---
+++ src-delomboked/main/java/org/spacious_team/table_wrapper/api/CellDataAccessObject.java
@@ -0,0 +1,215 @@
+/*
+ * Table Wrapper API
+ * Copyright (C) 2021  Spacious Team <spacious-team@ya.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.spacious_team.table_wrapper.api;
+
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Objects;
+
+import static java.util.Objects.requireNonNull;
+import static org.spacious_team.table_wrapper.api.CellDataAccessObjectHelper.*;
+
+/**
+ * @apiNote Impl may have parameters that affect how the value is parsed,
+ * for example DataTimeFormat that changes behavior of date time value parser.
+ */
+public interface CellDataAccessObject<C, R extends ReportPageRow> {
+
+    @Nullable
+    C getCell(R row, Integer cellIndex);
+
+    @Nullable
+    Object getValue(C cell);
+
+    /**
+     * @throws RuntimeException if method can't extract int value
+     */
+    default int getIntValue(C cell) {
+        return (int) getLongValue(cell);
+    }
+
+    /**
+     * @throws RuntimeException if method can't extract long value
+     */
+    default long getLongValue(C cell) {
+        @Nullable Object value = getValue(cell);
+        if (value instanceof Number) {
+            return ((Number) value).longValue();
+        } else if (value != null) {
+            String str = spacePattern.matcher(value.toString()).replaceAll("");
+            return Long.parseLong(str);
+        } else {
+            throw new NullPointerException(NO_CELL_VALUE_EXCEPTION_MESSAGE);
+        }
+    }
+
+    /**
+     * @throws RuntimeException if method can't extract Double value
+     */
+    default double getDoubleValue(C cell) {
+        @Nullable Object value = getValue(cell);
+        if (value instanceof Number) {
+            return ((Number) value).doubleValue();
+        } else if (value != null) {
+            String str = spacePattern.matcher(value.toString()).replaceAll("");
+            try {
+                return Double.parseDouble(str);
+            } catch (NumberFormatException e) {
+                if (str.indexOf(',') != -1) {
+                    return Double.parseDouble(str.replace(',', '.'));
+                }
+                throw e;
+            }
+        } else {
+            throw new NullPointerException(NO_CELL_VALUE_EXCEPTION_MESSAGE);
+        }
+    }
+
+    /**
+     * @throws RuntimeException if method can't extract BigDecimal value
+     * @see <a href="https://stackoverflow.com/questions/6787142/bigdecimal-equals-versus-compareto">Stack overflow</a>
+     * for BigDecimal values equality
+     */
+    default BigDecimal getBigDecimalValue(C cell) {
+        String number = getStringValue(cell);
+        number = spacePattern.matcher(number).replaceAll("");
+        number = number.replace(',', '.');
+        return (Objects.equals(number, "0") || Objects.equals(number, "0.0") || Objects.equals(number, "0.00")) ?
+                BigDecimal.ZERO : new BigDecimal(number);
+    }
+
+    /**
+     * @throws RuntimeException if method can't extract string value
+     */
+
+    default String getStringValue(C cell) {
+        @SuppressWarnings({"nullness", "ConstantConditions"})
+        Object value = requireNonNull(getValue(cell), "Not a string");
+        return value.toString();
+    }
+
+    /**
+     * @throws RuntimeException if method can't extract instant value
+     */
+    Instant getInstantValue(C cell);
+
+    /**
+     * Returns local date time at system default time zone.
+     *
+     * @throws RuntimeException if method can't extract local date time value
+     */
+    default LocalDateTime getLocalDateTimeValue(C cell) {
+        return getInstantValue(cell)
+                .atZone(defaultZoneId)
+                .toLocalDateTime();
+    }
+
+    /**
+     * @throws RuntimeException if method can't extract local date time value
+     */
+    default LocalDateTime getLocalDateTimeValue(C cell, ZoneId zoneId) {
+        return getInstantValue(cell)
+                .atZone(zoneId)
+                .toLocalDateTime();
+    }
+
+    default @Nullable Object getValue(R row, Integer cellIndex) {
+        @Nullable C cell = getCell(row, cellIndex);
+        return (cell == null) ? null : getValue(cell);
+    }
+
+    /**
+     * @throws RuntimeException if method can't extract int value
+     */
+    default int getIntValue(R row, Integer cellIndex) {
+        @SuppressWarnings({"nullness", "ConstantConditions"})
+        C cell = requireNonNull(getCell(row, cellIndex), "Cell is not found");
+        return getIntValue(cell);
+    }
+
+    /**
+     * @throws RuntimeException if method can't extract long value
+     */
+    default long getLongValue(R row, Integer cellIndex) {
+        @SuppressWarnings({"nullness", "ConstantConditions"})
+        C cell = requireNonNull(getCell(row, cellIndex), "Cell is not found");
+        return getLongValue(cell);
+    }
+
+    /**
+     * @throws RuntimeException if method can't extract Double value
+     */
+    default double getDoubleValue(R row, Integer cellIndex) {
+        @SuppressWarnings({"nullness", "ConstantConditions"})
+        C cell = requireNonNull(getCell(row, cellIndex), "Cell is not found");
+        return getDoubleValue(cell);
+    }
+
+    /**
+     * @throws RuntimeException if method can't extract BigDecimal value
+     */
+    default BigDecimal getBigDecimalValue(R row, Integer cellIndex) {
+        @SuppressWarnings({"nullness", "ConstantConditions"})
+        C cell = requireNonNull(getCell(row, cellIndex), "Cell is not found");
+        return getBigDecimalValue(cell);
+    }
+
+    /**
+     * @throws RuntimeException if method can't extract string value
+     */
+    default String getStringValue(R row, Integer cellIndex) {
+        @SuppressWarnings({"nullness", "ConstantConditions"})
+        C cell = requireNonNull(getCell(row, cellIndex), "Cell is not found");
+        return getStringValue(cell);
+    }
+
+    /**
+     * @throws RuntimeException if method can't extract instant value
+     */
+    default Instant getInstantValue(R row, Integer cellIndex) {
+        @SuppressWarnings({"nullness", "ConstantConditions"})
+        C cell = requireNonNull(getCell(row, cellIndex), "Cell is not found");
+        return getInstantValue(cell);
+    }
+
+    /**
+     * Returns local date time at default time zone.
+     *
+     * @throws RuntimeException if method can't extract local date time value
+     */
+    default LocalDateTime getLocalDateTimeValue(R row, Integer cellIndex) {
+        @SuppressWarnings({"nullness", "ConstantConditions"})
+        C cell = requireNonNull(getCell(row, cellIndex), "Cell is not found");
+        return getLocalDateTimeValue(cell);
+    }
+
+    /**
+     * @throws RuntimeException if method can't extract local date time value
+     */
+    @SuppressWarnings("UnusedReturnValue")
+    default LocalDateTime getLocalDateTimeValue(R row, Integer cellIndex, ZoneId zoneId) {
+        @SuppressWarnings({"nullness", "ConstantConditions"})
+        C cell = requireNonNull(getCell(row, cellIndex), "Cell is not found");
+        return getLocalDateTimeValue(cell, zoneId);
+    }
+}
---
+++ src-delomboked/main/java/org/spacious_team/table_wrapper/api/ReportPage.java
@@ -0,0 +1,452 @@
+/*
+ * Table Wrapper API
+ * Copyright (C) 2020  Spacious Team <spacious-team@ya.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.spacious_team.table_wrapper.api;
+
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.util.function.Predicate;
+
+import static java.util.Objects.requireNonNull;
+import static org.spacious_team.table_wrapper.api.ReportPageHelper.getCellStringValueIgnoreCasePrefixPredicate;
+
+@SuppressWarnings({"unused", "UnusedReturnValue"})
+public interface ReportPage {
+
+    /**
+     * Finds cell address containing exact value.
+     *
+     * @return cell address or {@link TableCellAddress#NOT_FOUND}
+     */
+    default TableCellAddress find(Object value) {
+        return find(value, 0);
+    }
+
+    /**
+     * Finds cell address containing exact value.
+     *
+     * @param startRow search rows start from this
+     * @return cell address or {@link TableCellAddress#NOT_FOUND}
+     */
+    default TableCellAddress find(Object value, int startRow) {
+        return find(value, startRow, Integer.MAX_VALUE);
+    }
+
+    /**
+     * Finds cell address containing exact value.
+     *
+     * @param startRow search rows start from this
+     * @param endRow   search rows excluding this, can handle values greater than real rows count
+     * @return cell address or {@link TableCellAddress#NOT_FOUND}
+     */
+    default TableCellAddress find(Object value, int startRow, int endRow) {
+        return find(value, startRow, endRow, 0, Integer.MAX_VALUE);
+    }
+
+    /**
+     * Finds cell address containing exact value.
+     *
+     * @param value       searching value
+     * @param startRow    search rows start from this
+     * @param endRow      search rows excluding this, can handle values greater than real rows count
+     * @param startColumn search columns start from this
+     * @param endColumn   search columns excluding this, can handle values greater than real columns count
+     * @return cell address or {@link TableCellAddress#NOT_FOUND}
+     */
+    TableCellAddress find(Object value, int startRow, int endRow, int startColumn, int endColumn);
+
+    /**
+     * Finds cell by predicate.
+     *
+     * @param cellValuePredicate predicate for testing cell value
+     * @return cell address or {@link TableCellAddress#NOT_FOUND}
+     */
+    default TableCellAddress find(Predicate<Object> cellValuePredicate) {
+        return find(0, cellValuePredicate);
+    }
+
+    /**
+     * Finds cell by predicate.
+     *
+     * @param startRow search rows start from this
+     * @return cell address or {@link TableCellAddress#NOT_FOUND}
+     */
+    default TableCellAddress find(int startRow, Predicate<Object> cellValuePredicate) {
+        return find(startRow, Integer.MAX_VALUE, cellValuePredicate);
+    }
+
+    /**
+     * Finds cell by predicate.
+     *
+     * @param startRow search rows start from this
+     * @param endRow   search rows excluding this, can handle values greater than real rows count
+     * @return cell address or {@link TableCellAddress#NOT_FOUND}
+     */
+    default TableCellAddress find(int startRow, int endRow, Predicate<Object> cellValuePredicate) {
+        return find(startRow, endRow, 0, Integer.MAX_VALUE, cellValuePredicate);
+    }
+
+    /**
+     * Finds cell by predicate.
+     *
+     * @param startRow           search rows start from this
+     * @param endRow             search rows excluding this, can handle values greater than real rows count
+     * @param startColumn        search columns start from this
+     * @param endColumn          search columns excluding this, can handle values greater than real columns count
+     * @param cellValuePredicate predicate for testing cell value
+     * @return cell address or {@link TableCellAddress#NOT_FOUND}
+     */
+    TableCellAddress find(int startRow, int endRow,
+                          int startColumn, int endColumn,
+                          Predicate<Object> cellValuePredicate);
+
+    /**
+     * Finds cell address staring with value (ignore case, trims leading spaces).
+     *
+     * @return cell address or {@link TableCellAddress#NOT_FOUND}
+     */
+    default TableCellAddress findByPrefix(String prefix) {
+        return findByPrefix(prefix, 0);
+    }
+
+    /**
+     * Finds cell address staring with value (ignore case, trims leading spaces).
+     *
+     * @param startRow search rows start from this
+     * @return cell address or {@link TableCellAddress#NOT_FOUND}
+     */
+    default TableCellAddress findByPrefix(String prefix, int startRow) {
+        return findByPrefix(prefix, startRow, Integer.MAX_VALUE);
+    }
+
+    /**
+     * Finds cell address staring with value (ignore case, trims leading spaces).
+     *
+     * @param startRow search rows start from this
+     * @param endRow   search rows excluding this, can handle values greater than real rows count
+     * @return cell address or {@link TableCellAddress#NOT_FOUND}
+     */
+    default TableCellAddress findByPrefix(String prefix, int startRow, int endRow) {
+        return findByPrefix(prefix, startRow, endRow, 0, Integer.MAX_VALUE);
+    }
+
+    /**
+     * Finds cell address staring with value (ignore case, trims leading spaces).
+     *
+     * @param startRow    search rows start from this
+     * @param endRow      search rows excluding this, can handle values greater than real rows count
+     * @param startColumn search columns start from this
+     * @param endColumn   search columns excluding this, can handle values greater than real columns count
+     * @return cell address or {@link TableCellAddress#NOT_FOUND}
+     */
+    default TableCellAddress findByPrefix(@Nullable String prefix, int startRow, int endRow, int startColumn, int endColumn) {
+        return (prefix == null || prefix.isEmpty()) ?
+                TableCellAddress.NOT_FOUND :
+                find(startRow, endRow, startColumn, endColumn, getCellStringValueIgnoreCasePrefixPredicate(prefix));
+    }
+
+    /**
+     * For vertical table of key-value records (table with two columns), search and return value for requested key.
+     */
+    default @Nullable Object getNextColumnValue(String firstColumnValuePrefix) {
+        TableCellAddress address = findByPrefix(firstColumnValuePrefix);
+        @Nullable ReportPageRow row = getRow(address.getRow());
+        if (row != null) {
+            for (@Nullable TableCell cell : row) {
+                if (cell != null && cell.getColumnIndex() > address.getColumn()) {
+                    @Nullable Object value = cell.getValue();
+                    if (value != null && (!(value instanceof String) || !((String) value).isBlank())) {
+                        return value;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @param i zero-based index
+     * @return row object or null is row does not exist
+     * @apiNote Method impl should return {@link CellDataAccessObject} aware {@link ReportPageRow} impl
+     */
+    @Nullable
+    ReportPageRow getRow(int i);
+
+    /**
+     * @return last row contained on this page (zero-based) or -1 if no row exists
+     */
+    int getLastRowNum();
+
+    default @Nullable TableCell getCell(TableCellAddress address) {
+        @Nullable ReportPageRow row = getRow(address.getRow());
+        return (row == null) ? null : row.getCell(address.getColumn());
+    }
+
+    /**
+     * Returns table range. Table's first row starts with 'firstRowPrefix' prefix in one of the cells
+     * and table ends with predefined prefix in one of the last row cells.
+     */
+    default TableCellRange getTableCellRange(@Nullable String firstRowPrefix,
+                                             int headersRowCount,
+                                             @Nullable String lastRowPrefix) {
+        if (firstRowPrefix == null || lastRowPrefix == null || firstRowPrefix.isEmpty() || lastRowPrefix.isEmpty()) {
+            return TableCellRange.EMPTY_RANGE;
+        }
+        return getTableCellRange(
+                getCellStringValueIgnoreCasePrefixPredicate(firstRowPrefix),
+                headersRowCount,
+                getCellStringValueIgnoreCasePrefixPredicate(lastRowPrefix));
+    }
+
+    /**
+     * Returns table range. First and last row will be found by predicate.
+     */
+    default TableCellRange getTableCellRange(@Nullable Predicate<Object> firstRowFinder,
+                                             int headersRowCount,
+                                             @Nullable Predicate<Object> lastRowFinder) {
+        if (firstRowFinder == null || lastRowFinder == null) {
+            return TableCellRange.EMPTY_RANGE;
+        }
+        TableCellAddress startAddress = find(firstRowFinder);
+        if (startAddress.equals(TableCellAddress.NOT_FOUND)) {
+            return TableCellRange.EMPTY_RANGE;
+        }
+        @SuppressWarnings({"nullness", "ConstantConditions"})
+        ReportPageRow firstRow = requireNonNull(getRow(startAddress.getRow()), "Row is not found");
+        TableCellAddress endAddress = find(startAddress.getRow() + headersRowCount + 1, lastRowFinder);
+        if (endAddress.equals(TableCellAddress.NOT_FOUND)) {
+            return TableCellRange.EMPTY_RANGE;
+        }
+        @SuppressWarnings({"nullness", "ConstantConditions"})
+        ReportPageRow lastRow = requireNonNull(getRow(endAddress.getRow()), "Row is not found");
+        return TableCellRange.of(
+                startAddress.getRow(),
+                endAddress.getRow(),
+                firstRow.getFirstCellNum(),
+                lastRow.getLastCellNum());
+    }
+
+    /**
+     * Returns table range. First row starts with 'firstRowPrefix' prefix in one of the cells,
+     * range ends with empty row or last row of report page.
+     */
+    default TableCellRange getTableCellRange(@Nullable String firstRowPrefix, int headersRowCount) {
+        if (firstRowPrefix == null || firstRowPrefix.isEmpty()) {
+            return TableCellRange.EMPTY_RANGE;
+        }
+        return getTableCellRange(
+                getCellStringValueIgnoreCasePrefixPredicate(firstRowPrefix),
+                headersRowCount);
+    }
+
+    /**
+     * Returns table range. First row will be found by predicate, range ends with empty row or last row of report page.
+     */
+    default TableCellRange getTableCellRange(@Nullable Predicate<Object> firstRowFinder, int headersRowCount) {
+        if (firstRowFinder == null) {
+            return TableCellRange.EMPTY_RANGE;
+        }
+        TableCellAddress startAddress = find(firstRowFinder);
+        if (startAddress.equals(TableCellAddress.NOT_FOUND)) {
+            return TableCellRange.EMPTY_RANGE;
+        }
+        @SuppressWarnings({"nullness", "ConstantConditions"})
+        ReportPageRow firstRow = requireNonNull(getRow(startAddress.getRow()), "Row is not found");
+        int emptyRowNum = findEmptyRow(startAddress.getRow() + headersRowCount + 1);
+        if (emptyRowNum == -1) {
+            emptyRowNum = getLastRowNum(); // empty row is not found, use last row
+        } else if (emptyRowNum <= getLastRowNum()) {
+            emptyRowNum--; // exclude empty row
+        }
+        ReportPageRow lastRow;
+        if (emptyRowNum <= startAddress.getRow()) {
+            emptyRowNum = startAddress.getRow();
+            lastRow = firstRow;
+        } else {
+            @SuppressWarnings({"nullness", "ConstantConditions"})
+            ReportPageRow row = requireNonNull(getRow(emptyRowNum), "Row is not found");
+            lastRow = row;
+        }
+        return TableCellRange.of(
+                startAddress.getRow(),
+                emptyRowNum,
+                firstRow.getFirstCellNum(),
+                lastRow.getLastCellNum());
+    }
+
+    /**
+     * Returns zero-based index of empty row.
+     * This implementation generates a huge amount of garbage. May be overridden for improve performance.
+     *
+     * @param startRow first row for check
+     * @return index of first empty row or -1 if empty row is not found
+     */
+    default int findEmptyRow(int startRow) {
+        int lastRowNum = startRow;
+        LAST_ROW:
+        for (int n = getLastRowNum(); lastRowNum <= n; lastRowNum++) {
+            @Nullable ReportPageRow row = getRow(lastRowNum);
+            if (row == null || row.getLastCellNum() == -1) {
+                return lastRowNum; // all row's cells are blank
+            }
+            for (@Nullable TableCell cell : row) {
+                @Nullable Object value;
+                if (!(cell == null
+                        || ((value = cell.getValue()) == null)
+                        || (value instanceof String) && (value.toString().isEmpty()))) {
+                    // not empty
+                    continue LAST_ROW;
+                }
+            }
+            return lastRowNum; // all row's cells are blank
+        }
+        return -1;
+    }
+
+    default <T extends Enum<T> & TableHeaderColumn>
+    Table create(String tableName,
+                 String tableFooterString,
+                 Class<T> headerDescription) {
+        return TableFactoryRegistry.get(this)
+                .create(this, tableName, tableFooterString, headerDescription);
+    }
+
+    default <T extends Enum<T> & TableHeaderColumn>
+    Table create(String tableName,
+                 Class<T> headerDescription) {
+        return TableFactoryRegistry.get(this)
+                .create(this, tableName, headerDescription);
+    }
+
+    default <T extends Enum<T> & TableHeaderColumn>
+    Table create(String tableName,
+                 String tableFooterString,
+                 Class<T> headerDescription,
+                 int headersRowCount) {
+        return TableFactoryRegistry.get(this)
+                .create(this, tableName, tableFooterString, headerDescription, headersRowCount);
+    }
+
+    default <T extends Enum<T> & TableHeaderColumn>
+    Table create(String tableName,
+                 Class<T> headerDescription,
+                 int headersRowCount) {
+        return TableFactoryRegistry.get(this)
+                .create(this, tableName, headerDescription, headersRowCount);
+    }
+
+    default <T extends Enum<T> & TableHeaderColumn>
+    Table create(Predicate<Object> tableNameFinder,
+                 Predicate<Object> tableFooterFinder,
+                 Class<T> headerDescription) {
+        return TableFactoryRegistry.get(this)
+                .create(this, tableNameFinder, tableFooterFinder, headerDescription);
+    }
+
+    default <T extends Enum<T> & TableHeaderColumn>
+    Table create(Predicate<Object> tableNameFinder,
+                 Class<T> headerDescription) {
+        return TableFactoryRegistry.get(this)
+                .create(this, tableNameFinder, headerDescription);
+    }
+
+    default <T extends Enum<T> & TableHeaderColumn>
+    Table create(Predicate<Object> tableNameFinder,
+                 Predicate<Object> tableFooterFinder,
+                 Class<T> headerDescription,
+                 int headersRowCount) {
+        return TableFactoryRegistry.get(this)
+                .create(this, tableNameFinder, tableFooterFinder, headerDescription, headersRowCount);
+    }
+
+    default <T extends Enum<T> & TableHeaderColumn>
+    Table create(Predicate<Object> tableNameFinder,
+                 Class<T> headerDescription,
+                 int headersRowCount) {
+        return TableFactoryRegistry.get(this)
+                .create(this, tableNameFinder, headerDescription, headersRowCount);
+    }
+
+    default <T extends Enum<T> & TableHeaderColumn>
+    Table createNameless(String firstLineText,
+                         String lastRowString,
+                         Class<T> headerDescription) {
+        return TableFactoryRegistry.get(this)
+                .createNameless(this, firstLineText, lastRowString, headerDescription);
+    }
+
+    default <T extends Enum<T> & TableHeaderColumn>
+    Table createNameless(String firstLineText,
+                         Class<T> headerDescription) {
+        return TableFactoryRegistry.get(this)
+                .createNameless(this, firstLineText, headerDescription);
+    }
+
+    default <T extends Enum<T> & TableHeaderColumn>
+    Table createNameless(String providedTableName,
+                         String firstLineText,
+                         String lastRowString,
+                         Class<T> headerDescription,
+                         int headersRowCount) {
+        return TableFactoryRegistry.get(this)
+                .createNameless(this, providedTableName, firstLineText, lastRowString, headerDescription, headersRowCount);
+    }
+
+    default <T extends Enum<T> & TableHeaderColumn>
+    Table createNameless(String providedTableName,
+                         String firstLineText,
+                         Class<T> headerDescription,
+                         int headersRowCount) {
+        return TableFactoryRegistry.get(this)
+                .createNameless(this, providedTableName, firstLineText, headerDescription, headersRowCount);
+    }
+
+    default <T extends Enum<T> & TableHeaderColumn>
+    Table createNameless(Predicate<Object> firstLineFinder,
+                         Predicate<Object> lastRowFinder,
+                         Class<T> headerDescription) {
+        return TableFactoryRegistry.get(this)
+                .createNameless(this, firstLineFinder, lastRowFinder, headerDescription);
+    }
+
+    default <T extends Enum<T> & TableHeaderColumn>
+    Table createNameless(Predicate<Object> firstLineFinder,
+                         Class<T> headerDescription) {
+        return TableFactoryRegistry.get(this)
+                .createNameless(this, firstLineFinder, headerDescription);
+    }
+
+    default <T extends Enum<T> & TableHeaderColumn>
+    Table createNameless(String providedTableName,
+                         Predicate<Object> firstLineFinder,
+                         Predicate<Object> lastRowFinder,
+                         Class<T> headerDescription,
+                         int headersRowCount) {
+        return TableFactoryRegistry.get(this)
+                .createNameless(this, providedTableName, firstLineFinder, lastRowFinder, headerDescription, headersRowCount);
+    }
+
+    default <T extends Enum<T> & TableHeaderColumn>
+    Table createNameless(String providedTableName,
+                         Predicate<Object> firstLineFinder,
+                         Class<T> headerDescription,
+                         int headersRowCount) {
+        return TableFactoryRegistry.get(this)
+                .createNameless(this, providedTableName, firstLineFinder, headerDescription, headersRowCount);
+    }
+}
